9871cbbd15f32165eda0ae956d5b229ca7c06b2c
Farrel Putra <70509726+thundergaming349@users.noreply.github.com>
2026-01-08 22:54:05 +0700

    Create template-header.blade.php

---
+++ template-header.blade.php
@@ -0,0 +1,4 @@
+    <header class="flex justify-between p-5 bg-gray-800">
+        <h1 class="text-2xl font-bold">Yoko Film</h1>
+        <button class="bg-blue-600 px-4 py-2 rounded hover: bg-blue-500">Register</button>
+    </header>
