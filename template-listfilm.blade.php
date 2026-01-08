6370eabeb709a16dbca8f7af2296743ad01b7aef
Farrel Putra <70509726+thundergaming349@users.noreply.github.com>
2026-01-08 22:55:48 +0700

    Create template-listfilm.blade.php

---
+++ template-listfilm.blade.php
@@ -0,0 +1,37 @@
+     $movie = [
+            [
+                'name' => 'Sonic The Hedgehog 3',
+                'director' => 'Jeff Fowler',
+                'year' => 2024,
+                'duration' => 110,
+                'poster' => 'https://media.themoviedb.org/t/p/w300_and_h450_face/d8Ryb8AunYAuycVKDp5HpdWPKgC.jpg'
+            ],
+            [
+                'name' => 'Zootopia',
+                'director' => 'Rich Moore',
+                'year' => 2016,
+                'duration' => 109,
+                'poster' => 'https://media.themoviedb.org/t/p/w300_and_h450_face/hlK0e0wAQ3VLuJcsfIYPvb4JVud.jpg'
+            ],
+            [
+                'name' => 'Agak Laen: Menyala Pantiku',
+                'director' => 'Muhadkly Acho',
+                'year' => 2025,
+                'duration' => 120,
+                'poster' => 'https://media.themoviedb.org/t/p/w300_and_h450_face/nMJqvtrgX6dqZNcNfAJvS5o7wd1.jpg'
+            ],
+            [
+                'name' => 'Elemental',
+                'director' => 'Peter Sohn',
+                'year' => 2023,
+                'duration' => 104,
+                'poster' => 'https://media.themoviedb.org/t/p/w300_and_h450_face/4Y1WNkd88JXmGfhtWR7dmDAo1T2.jpg'
+            ],
+            [
+                'name' => 'Planes',
+                'director' => 'Klay Hall',
+                'year' => 2013,
+                'duration' => 94,
+                'poster' => 'https://media.themoviedb.org/t/p/w300_and_h450_face/i2xgU0y0p77WTrB0oIkbpdaWq8R.jpg'
+            ]
+        ];
