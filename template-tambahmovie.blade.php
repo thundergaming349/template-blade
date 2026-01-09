44d4a44d2108a92fc4b4841093a03487c75bb416
Farrel Putra <70509726+thundergaming349@users.noreply.github.com>
2026-01-09 17:03:57 +0700

    Create template-tambahmovie.blade.php

---
+++ template-tambahmovie.blade.php
@@ -0,0 +1,44 @@
+@extends('app')
+
+
+@section('content')
+<div  class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <h2 class="text-2xl font-bold mb-6">Tambah Film</h2>
+
+    <form action="" class="space-y-6">
+        <div>
+            <label for="judul" class="block text-lg mb-2">Judul </label>
+            <input type="text" name="judul" id="judul" class="w-full p-2 bg-gray-800 border border-gray-700 rounded focus:outline-none focus:ring-2 focus:ring-blue-600">
+        </div>
+        <div>
+            <label for="director" class="block text-lg mb-2">Director</label>
+            <input type="text" name="director" id="director" class="w-full p-2 bg-gray-800 border border-gray-700 rounded focus:outline-none focus:ring-2 focus:ring-blue-600">
+        </div>
+        <div>
+            <label for="year" class="block text-lg mb-2">Tahun</label>
+            <input type="text" name="year" id="year" class="w-full p-2 bg-gray-800 border border-gray-700 rounded focus:outline-none focus:ring-2 focus:ring-blue-600">
+        </div>
+        <div>
+            <label for="duration" class="block text-lg mb-2">Durasi</label>
+            <input type="text" name="duration" id="duration" class="w-full p-2 bg-gray-800 border border-gray-700 rounded focus:outline-none focus:ring-2 focus:ring-blue-600">
+        </div>
+        <div>
+            <label for="synopsis" class="block text-lg mb-2">Sinopsis</label>
+            <textarea name="synopsis" id="synopsis" cols="20" rows="5" class="w-full p-2 bg-gray-800 border border-gray-700 rounded focus:outline-none focus:ring-2 focus:ring-blue-600"></textarea>
+        </div>
+        <div>
+            <label for="genre" class="block text-lg mb-2">Genre</label>
+            <input type="text" name="genre" id="genre" class="w-full p-2 bg-gray-800 border border-gray-700 rounded focus:outline-none focus:ring-2 focus:ring-blue-600">
+        </div>
+        <div>
+            <label for="poster" class="block text-lg mb-2">Poster</label>
+            <input type="text" name="poster" id="poster" class="w-full p-2 bg-gray-800 border border-gray-700 rounded focus:outline-none focus:ring-2 focus:ring-blue-600">
+        </div>
+
+        <div>
+            <button type="submit" class="bg-blue-600 px-6 py-2 rounded hover:bg-blue-500">Simpan</button>
+        </div>
+    </form>
+</div>
+    
+@endsection
