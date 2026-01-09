c9d3e7e0dddb70b4972f12c506c0988beff52214
Farrel Putra <70509726+thundergaming349@users.noreply.github.com>
2026-01-09 13:42:29 +0700

    Update template-listfilm.blade.php

--- template-listfilm.blade.php
+++ template-listfilm.blade.php
@@ -1,37 +1,60 @@
-     $movie = [
+        $movie = [
             [
                 'name' => 'Sonic The Hedgehog 3',
                 'director' => 'Jeff Fowler',
                 'year' => 2024,
                 'duration' => 110,
-                'poster' => 'https://media.themoviedb.org/t/p/w300_and_h450_face/d8Ryb8AunYAuycVKDp5HpdWPKgC.jpg'
+                'poster' => 'https://media.themoviedb.org/t/p/w300_and_h450_face/d8Ryb8AunYAuycVKDp5HpdWPKgC.jpg',
+                'synopsis' => 'Sonic, Knuckles, dan Tails bersatu kembali melawan musuh baru yang kuat, 
+                            Shadow, penjahat misterius dengan kekuatan yang belum pernah mereka hadapi sebelumnya. 
+                            Dengan kemampuan mereka yang tak tertandingi dalam segala hal, Tim Sonic harus mencari 
+                            aliansi yang tidak terduga dengan harapan dapat menghentikan Shadow dan 
+                            melindungi planet ini.',
+                'genre' => ['Action', 'Comedy', 'Fiction']
             ],
             [
                 'name' => 'Zootopia',
                 'director' => 'Rich Moore',
                 'year' => 2016,
                 'duration' => 109,
-                'poster' => 'https://media.themoviedb.org/t/p/w300_and_h450_face/hlK0e0wAQ3VLuJcsfIYPvb4JVud.jpg'
+                'poster' => 'https://media.themoviedb.org/t/p/w300_and_h450_face/hlK0e0wAQ3VLuJcsfIYPvb4JVud.jpg',
+                'synopsis' => 'Bertekad untuk membuktikan dirinya, Petugas Judy Hopps, kelinci pertama di 
+                            kepolisian Zootopia, melompat pada kesempatan untuk memecahkan kasus pertamanya 
+                            bahkan jika itu berarti bermitra dengan penipu rubah Nick Wilde untuk memecahkan misteri.',
+                'genre' => ['Animation', 'Mystery', 'Adventure']
             ],
             [
                 'name' => 'Agak Laen: Menyala Pantiku',
                 'director' => 'Muhadkly Acho',
                 'year' => 2025,
                 'duration' => 120,
-                'poster' => 'https://media.themoviedb.org/t/p/w300_and_h450_face/nMJqvtrgX6dqZNcNfAJvS5o7wd1.jpg'
+                'poster' => 'https://media.themoviedb.org/t/p/w300_and_h450_face/nMJqvtrgX6dqZNcNfAJvS5o7wd1.jpg',
+                'synopsis' => 'Setelah berulang kali gagal dalam misi mereka, Detektif Bene, Boris, Jegel, dan 
+                            Oki diberi satu kesempatan terakhir: untuk menyamar di sebuah panti jompo untuk 
+                            mencari buronan yang dicari karena pembunuhan anak walikota.',
+                'genre' => ['Comedy', 'Drama', 'Family']
             ],
             [
                 'name' => 'Elemental',
                 'director' => 'Peter Sohn',
                 'year' => 2023,
                 'duration' => 104,
-                'poster' => 'https://media.themoviedb.org/t/p/w300_and_h450_face/4Y1WNkd88JXmGfhtWR7dmDAo1T2.jpg'
+                'poster' => 'https://media.themoviedb.org/t/p/w300_and_h450_face/4Y1WNkd88JXmGfhtWR7dmDAo1T2.jpg',
+                'synopsis' => 'Di kota tempat tinggal bersama penghuni api, air, darat, dan udara, seorang 
+                            wanita muda yang berapi-api dan seorang pria yang santai akan menemukan sesuatu 
+                            yang mendasar: betapa banyak kesamaan yang mereka miliki.',
+                'genre' => ['Fantasy', 'Comedy', 'Romance']
             ],
             [
                 'name' => 'Planes',
                 'director' => 'Klay Hall',
                 'year' => 2013,
                 'duration' => 94,
-                'poster' => 'https://media.themoviedb.org/t/p/w300_and_h450_face/i2xgU0y0p77WTrB0oIkbpdaWq8R.jpg'
+                'poster' => 'https://media.themoviedb.org/t/p/w300_and_h450_face/i2xgU0y0p77WTrB0oIkbpdaWq8R.jpg',
+                'synopsis' => 'Dusty adalah pesawat penyemprot tanaman yang bermimpi untuk berkompetisi dalam 
+                            perlombaan udara terkenal. Masalahnya? Dia sangat takut ketinggian. Dengan dukungan 
+                            dari mentornya, Skipper, dan sejumlah teman baru, Dusty berangkat untuk 
+                            mewujudkan mimpinya.',
+                'genre' => ['Animation', 'Family', 'Adventure']
             ]
         ];
