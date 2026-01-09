9e086f1b4f2f084e19af94f82c61b7a0d5ef2b5a
Farrel Putra <70509726+thundergaming349@users.noreply.github.com>
2026-01-09 20:24:05 +0700

    Update template-home.blade.php

--- template-home.blade.php
+++ template-home.blade.php
@@ -3,7 +3,9 @@
 @section('content')
     <div class="border-t border-b border-gray-500 text-white mb-4">
         <div class="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
-            <h1 class="text-4xl font-bold text-center">Daftar Film</h1>
+            <h1 class="text-4xl font-bold text-center mb-4">Daftar Film</h1>
+            <input type="text" name="search" id="search" placeholder="🔍︎ Mau nonton apa hari ini?"
+                class="w-full p-2 bg-gray-800 border border-gray-700 rounded focus:outline-none focus:ring-2 focus:ring-blue-600">
         </div>
     </div>
 
@@ -18,7 +20,8 @@
 
 
                     <div class="absolute top-2 right-2 space-x-2 opacity-0 group-hover:opacity-100 transition">
-                        <a href="" class="bg-green-600 p-1 rounded hover:bg-green-100">✏️</a>
+                        <a href="{{ route('edit', $loop->index) }}"
+                            class="bg-green-600 p-1 rounded hover:bg-green-100">✏️</a>
                         <button class="bg-red-600 p-1 rounded hover:bg-red-100">🗑️</button>
                     </div>
                 </a>
@@ -28,9 +31,8 @@
     <hr class="text-gray-200 opacity-70 mb-7 mt-10">
     <div class="w-full align-center">
         <p class="text-white-400 text-lg mb-2">ingin tambah film?</p>
-        <a href="{{route('create')}}">
-        <button class="bg-blue-600 px-4 py-2 align-center rounded hover:bg-blue-500">Tambah Film</button>
+        <a href="{{ route('create') }}">
+            <button class="bg-blue-600 px-4 py-2 align-center rounded hover:bg-blue-500">Tambah Film</button>
         </a>
     </div>
-
 @endsection
